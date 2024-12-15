b06eaf75e1aaa81a16f612522da65282b4370ba3
Prem Soni <premsoni0469@gmail.com>
2024-12-15 10:49:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function reverseStr(){
+var inputStr = prompt("Enter string for reversal");
+    if(true){
+        confirm("Do you want to proceed?");
+        if(true)
+            var reverseStr = inputStr.split('').reverse().join('');
+            alert("Reversed string: "+ reverseStr);
+    }
+}
